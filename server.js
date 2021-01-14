e810ccba6743df2cbe84e336eb8ea0ac5a441539
olliepayne <olliepayne@me.com>
2021-01-13 19:54:58 -0700

    tweak port

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const cors = require('cors')
 const booksRouter = require('./routes/books')
 
 const app = express()
+const port = process.env.PORT || 3001
 
 require('dotenv').config();
 require('./config/database');
 
+app.use(cors())
+app.use(express.json())
+
 app.use('/books', booksRouter)
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(port, () => {
   console.log('Connected, server listening on port 3001...')
 })
